02c14db4e6a3e11eb45b4c68f12fb8e1ba122b1e
zhaojian <zhaojian@eltc.cn>
2022-12-19 17:29:24 +0800

    api请求地址中有undefined问题

--- src/hooks/setting/index.ts
+++ src/hooks/setting/index.ts
@@ -23,7 +23,7 @@ export const useGlobSetting = (): Readonly<GlobConfig> => {
     // title: VITE_GLOB_APP_TITLE,
     apiUrl: VITE_GLOB_API_URL,
     // shortName: VITE_GLOB_APP_SHORT_NAME,
-    // urlPrefix: VITE_GLOB_API_URL_PREFIX,
+    urlPrefix: VITE_GLOB_API_URL_PREFIX,
     // uploadUrl: VITE_GLOB_UPLOAD_URL,
   };
   return glob as Readonly<GlobConfig>;
